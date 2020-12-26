77e16675759529eb32be708354cc2c207d2a1ec4
林悠暉 <mrchildreny@gmail.com>
2020-12-27 03:01:16 +0900

    add button component

---
+++ src/components/Atoms/BaseButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const _Button = styled.button`
+  position: relative;
+  border: 0;
+  cursor: pointer;
+  border-radius: 2px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: white;
+  background-color: ${props=> props.color };
+  transition: background-color 0.2s ease,
+    box-shadow 0.28s cubic-bezier(0.4, 0, 0.2, 1);
+  outline: none;
+  height: 37px;
+  min-width: 90px;
+  border-radius: 2px;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    -webkit-transform: translate(0, 2px);
+    -moz-transform: translate(0, 2px);
+    transform: translate(0, 2px);
+  }
+`
+
+export const BaseButton = ({ children, color }: any) => {
+  return (
+    <_Button color={color}>
+      { children }
+    </_Button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from "react";
 import styled from 'styled-components';
+import { BaseButton } from '../components/Atoms/BaseButton'
 
 const _Container = styled.div`
   margin: 0 24px;
@@ -11,6 +12,7 @@ const Index = () => {
   return (
     <_Container>
       <h1>next.js</h1>
+      <BaseButton color="black">ボタン</BaseButton>
     </_Container>
   )
 }
