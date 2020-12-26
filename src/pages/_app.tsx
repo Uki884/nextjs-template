d7d1456751e891fe5a57682b22a747b8c473311f
林悠暉 <mrchildreny@gmail.com>
2020-12-27 01:56:12 +0900

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import App, { Container } from "next/app";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from "react";
+import styled from 'styled-components';
+
+const _Container = styled.div`
+  margin: 0 24px;
+  text-align: left;
+`
+
+const Index = () => {
+  return (
+    <_Container>
+      <h1>next.js</h1>
+    </_Container>
+  )
+}
+export default Index
