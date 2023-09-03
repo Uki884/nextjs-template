a6257891e4a7d08427e293e5b04ba45301b2d9ae
Yuki Hayashi <mrchildreny@gmail.com>
2023-09-03 11:34:56 +0900

    vanilla-extractを削除

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const path = require('path')
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-const withVanillaExtract = createVanillaExtractPlugin();
 
-const config = {
+export default {
   webpack(config, options) {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config
   },
-}
-
-module.exports = withVanillaExtract(config);
+};
